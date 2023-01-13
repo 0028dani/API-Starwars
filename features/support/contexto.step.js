9ce5c9b2c0a7707984723d454a30dad4a0263842
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2023-01-13 13:53:28 -0300

    contexto

---
+++ features/support/contexto.step.js
@@ -0,0 +1,37 @@
+const axios = require("axios").default;
+const { setWorldConstructor } = require("@cucumber/cucumber");
+const { default: json_formatter } = require("cucumber/lib/formatter/json_formatter");
+
+class Contexto {
+    async acessarFilme() {
+       var {pessoa} = this;
+       var url = `https://swapi.dev/api/people/${pessoa}`;
+       
+       var response = await axios.get(url);
+    console.log(" API dos filmes " + JSON.stringify(response.data.films))
+       return {
+         ...response.data, filmes: response.data.films
+       }
+    }
+
+    async filme(filme){
+        var url = filme;
+        var response = await axios.get(url);
+        console.log(" informações do filme: " + JSON.stringify(response.data.title))
+
+        return {
+            ...response.data, title: response.data.title
+        }
+    }
+
+    async planets(){
+        var {nomes} = this;
+        var url = `https://swapi.dev/api/planets/${nomes}`;
+        var response = await axios.get(url);
+        console.log("retorno: " + JSON.stringify(response.data))
+        return {
+            ...response.data
+        }
+    }
+ }
+ setWorldConstructor(Contexto)
